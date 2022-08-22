c93f9976590c9e51a7b86a9f6a4b8133a0da6428
“ian-ma-nyc” <sinma1995@gmail.com>
2022-08-22 16:38:21 -0400

    deployment test

--- dockervize/src/components/TeamMembers.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logo from '../assets/wordpressTransparent.png';
-
-const TeamMembers = () => {
-  return <img src={logo} alt='logo' />;
-};
-
-export default TeamMembers;
