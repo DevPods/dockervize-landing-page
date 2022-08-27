f0648099f71516901a4e22ef2b11d11ce7b89e7a
“ian-ma-nyc” <sinma1995@gmail.com>
2022-08-26 22:56:40 -0400

    travis test

--- src/components/MemberCards.jsx
+++ src/components/MemberCards.jsx
@@ -22,7 +22,7 @@ const TeamMembers = () => {
           <p className='member-bio'>
             I am a software engineer and my passion is building unique projects.
             When I am not coding you can find me playing volleyball, spoiling my
-            cat, or at boardgame night!
+            cat, or at a boardgame night!
           </p>
 
           <p className='logos'>
